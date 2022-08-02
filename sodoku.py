a033437068d7f11a41c42631c77ea9d80e4717cf
Aidan Keighron <aidankeighron@gmail.com>
2022-08-02 15:02:39 -0500

    board

--- sodoku.py
+++ sodoku.py
@@ -1,39 +1,47 @@
 import random
+import numpy as np
 
-board = [[0] * 9] * 9
-visible = [[0] * 9] * 9
+board = np.zeros((9, 9))
+visible = np.zeros((9, 9))
+sections = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
     
-def generate_board():
-    for i in len(board):
-        for j in len(board[j]):
+def generate_board(show=False):
+    for c in range(len(board)):
+        for r in range(len(board[c])):
             condition = True
             number = 0
             while condition:
-                number = random.randint(1, 10)
-                if check(0, i, number) and check(1, j, number) and check(2, i, number): # Section
+                for l in board:
+                    print(l)
+                number = random.randint(1, 9)
+                if check(0, c, number) and check(1, r, number) and check(2, sections[int((c)/3)][int((r)/3)], number):
                     condition = False
-            board[i][j] = number
+            board[c][r] = number
+    if show:
+        for l in board:
+            print(l)
+                
             
-    print(board)
 # 0 = row
 # 1 = colum
 # 2 = box
 def check(axis, index, number):
     if axis == 0:
-        for i in len(board):
-            if board[i][index] == number:
-                return True
-    if axis == 1:
-        for i in len(board[index]):
+        for i in range(len(board[index])):
             if board[index][i] == number:
-                return True
+                return False
+    if axis == 1:
+        for i in range(len(board)):
+            if board[i][index] == number:
+                return False
     if axis == 2:
-        section = board[0:3][0:3] # Split
-        for i in len(section):
-            for j in len(section[j]):
+        section = board[(3-(3-int((index-1)/3)*3)):((3-(3-int((index-1)/3)*3))+3),(((index-(3*((int((index-1)/3)+1)-1)))*3)-3):((index-(3*((int((index-1)/3)+1)-1)))*3)]
+        for i in range(len(section)):
+            for j in range(len(section[i])):
                 if section[i][j] == number:
                     return False
-    return False
+    return True
         
-    
-generate_board()
+generate_board(True)
